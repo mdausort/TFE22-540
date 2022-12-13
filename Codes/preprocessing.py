fed78e7a10659156e77064dc961b543c118ab71d
Manon Dausort <manon.dausort@student.uclouvain.be>
2022-12-13 15:03:49 +0100

    Images du READme

--- Codes/preprocessing.py
+++ Codes/preprocessing.py
@@ -5,9 +5,9 @@ from elikopy.individual_subject_processing import report_solo
 f_path="/CECI/proj/pilab/PermeableAccess/alcoolique_TnB32xGDr7h/alcoholic_study/" 
 dic_path="/home/users/q/d/qdessain/Script_python/fixed_rad_dist.mat"
 
-patient_list = None#["sub02_T1", "sub04_T1", "sub05_T1", "sub08_T1", "sub09_T1", "sub11_T1", "sub12_T1", "sub13_T1", "sub14_T1", "sub15_T1", "sub17_T1", "sub18_T1", "sub19_T1", "sub20_T1", "sub21_T1", "sub22_T1", "sub24_T1", "sub26_T1", "sub27_T1", "sub28_T1", "sub30_T1", "sub31_T1", "sub32_T1", "sub33_T1", "sub34_T1", "sub35_T1", "sub36_T1", "sub37_T1", "sub39_T1", "sub40_T1", "sub41_T1", "sub42_T1", "sub43_T1", "sub45_T1", "sub46_T1", "sub02_T2", "sub04_T2", "sub05_T2", "sub08_T2", "sub09_T2", "sub11_T2", "sub12_T2", "sub13_T2", "sub14_T2", "sub15_T2", "sub17_T2", "sub18_T2", "sub19_T2", "sub20_T2", "sub21_T2", "sub22_T2", "sub24_T2", "sub26_T2", "sub27_T2", "sub28_T2", "sub30_T2", "sub31_T2", "sub32_T2", "sub33_T2", "sub34_T2", "sub35_T2", "sub36_T2", "sub37_T2", "sub39_T2", "sub40_T2", "sub41_T2", "sub42_T2", "sub43_T2", "sub45_T2", "sub46_T2"]
+patient_list = None
                                    
-study = elikopy.core.Elikopy(f_path, slurm=True, slurm_email='manon.dausort@student.uclouvain.be', cuda=False)
+study = elikopy.core.Elikopy(f_path, slurm=True, slurm_email='manon.dausort@uclouvain.be', cuda=False)
 
 # =============================================================================
 # Patient list
@@ -30,12 +30,12 @@ study.patient_list()
 #               report=True)
 
 # =============================================================================
-# Mask de matière blanche
+# Mask of white matter
 # =============================================================================
 # study.white_mask(patient_list_m=patient_list, corr_gibbs=True, cpus=2, debug=False) 
    
 # =============================================================================
-# Modèles microstructuraux 
+# Microstructural models
 # =============================================================================
 # study.dti(patient_list_m=patient_list)
 # study.noddi(use_wm_mask=False, patient_list_m=patient_list, cpus=4)
@@ -43,7 +43,7 @@ study.patient_list()
 # study.diamond(patient_list_m=patient_list,slurm_timeout="30:00:00",cpus=8)
 
 # =============================================================================
-# Statistiques
+# Stats
 # =============================================================================
 # grp1=[1]
 # grp2=[2]
